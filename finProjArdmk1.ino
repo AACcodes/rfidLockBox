800761347e9aec20c60dcd61550648b290c67608
Ayush Chaudhary <ayushc.pro@gmail.com>
2022-04-28 21:37:48 -0400

    a

--- finProjArdmk1.ino
+++ finProjArdmk1.ino
@@ -17,7 +17,9 @@ Servo servo;
 //const int togglePin = 2; 
 
 boolean locked = false;
-char input;
+String input;
+String password = "stupidESP";
+boolean writeCard = false;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -46,10 +48,10 @@ void setup() {
 void updateLock() {
     if (locked) {
         servo.write(180);
-        delay(100);
+        //delay(100);
     } else {
         servo.write(90);
-        delay(100);
+        //delay(100);
     }
 }
 
@@ -57,17 +59,20 @@ void updateLock() {
 //RFID globals
 byte sector         = 1;
 byte blockAddr      = 4;
-byte dataBlock[]    = {
-        0x01, 0x02, 0x03, 0x04, //  1,  2,   3,  4,
-        0x05, 0x06, 0x07, 0x08, //  5,  6,   7,  8,
-        0x09, 0x0a, 0xff, 0x0b, //  9, 10, 255, 11,
-        0x0c, 0x0d, 0x0e, 0x0f  // 12, 13, 14, 15
-    };
+
 byte trailerBlock   = 7;
 MFRC522::StatusCode status;
 byte buffer[18];
 byte size = sizeof(buffer);
 
+// Password used to unlock the box
+byte boxKey[]   = {
+        0x41, 0x41, 0x41, 0x41,
+        0x41, 0x41, 0x41, 0x41,
+        0x41, 0x41, 0x41, 0x41,
+        0x41, 0x41, 0x41, 0x41,
+    };
+ 
 
 
 void readRFID(){
@@ -78,6 +83,8 @@ void readRFID(){
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("PCD_Authenticate() failed: "));
         Serial.println(mfrc522.GetStatusCodeName(status));
+        mfrc522.PICC_HaltA();
+        mfrc522.PCD_StopCrypto1();
         return;
     }
   // Read data from the block
@@ -87,8 +94,12 @@ void readRFID(){
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("MIFARE_Read() failed: "));
         Serial.println(mfrc522.GetStatusCodeName(status));
+        mfrc522.PICC_HaltA();
+        mfrc522.PCD_StopCrypto1();
+        return;
     }
-    locked = !locked;
+    //locked = !locked;
+    auth();
     Serial.print(F("Data in block ")); Serial.print(blockAddr); Serial.println(F(":"));
     dump_byte_array(buffer, 16); Serial.println();
     Serial.println();
@@ -97,21 +108,82 @@ void readRFID(){
     Serial.println("Read done");
 }
 
+void programCard(){
+  //auth using A
+  // Authenticate using key A
+    Serial.println(F("Authenticating using key A..."));
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("PCD_Authenticate() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        mfrc522.PICC_HaltA();
+        mfrc522.PCD_StopCrypto1();
+        return;
+    }
+
+   //write data to block
+   Serial.print(F("Writing data into block ")); Serial.print(blockAddr);
+   Serial.println(F(" ..."));
+   dump_byte_array(boxKey, 16); Serial.println();
+   status = (MFRC522::StatusCode) mfrc522.MIFARE_Write(blockAddr, boxKey, 16);
+   if (status != MFRC522::STATUS_OK) {
+        Serial.print(F("MIFARE_Write() failed: "));
+        Serial.println(mfrc522.GetStatusCodeName(status));
+        mfrc522.PICC_HaltA();
+        mfrc522.PCD_StopCrypto1();
+        return;
+   }
+   Serial.println();
+   mfrc522.PICC_HaltA();
+   mfrc522.PCD_StopCrypto1();
+   Serial.println("Write done");
+
+  
+}
+
+void auth(){
+  int count = 0;
+    for (int i = 0; i < 16; i++) {
+        // Compare buffer (= what we've read) with dataBlock (= what we've written)
+        if (buffer[i] == boxKey[i])
+            count++;
+    }
+    if(count == 16){
+      Serial.println("Auth correct");
+      locked = !locked;
+    }else{
+      Serial.println("Auth incorrect");
+    }
+  
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 
   //manual override with serial typing
   while(Serial.available()){
-        input = Serial.read();
+        input = Serial.readStringUntil('\n');
         Serial.print("You typed: " );
         Serial.println(input);
-        locked = !locked;
+        if(input == password){
+          Serial.println("Auth correct. Lock toggled");
+          locked = !locked;
+        }else if(input == "write"){
+          Serial.println("THE NEXT CARD YOU PRESENT WILL BE WRITTEN TO");
+          writeCard = true;
+        }
     }
 
   // RFID chip in use
    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+        if(writeCard){
+          writeCard = false;
+          programCard();
+        }else{
         readRFID();
-        delay(600);
+        }
+        delay(800);
+        //Delay to prevant too fast rescan
    }
   /* Button code
   int buttonState = digitalRead(togglePin);
